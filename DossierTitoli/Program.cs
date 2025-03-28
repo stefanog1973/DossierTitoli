49a2013668e63cbb477b280a404b511ed15368fd
stefano grippa <stefanog1973@gmail.com>
2025-03-28 17:37:23 +0100

    connessione a db

--- DossierTitoli/Program.cs
+++ DossierTitoli/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
+using System.Data.OleDb;
+
 Console.WriteLine("Hello, World!");
+
+var conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\grippst1\\OneDrive - Reti\\Documents\\Databasestetitoli.mdb;");
+conn.Open();
+var cmd = new OleDbCommand("SELECT * FROM DossierTitoli", conn);
+var reader = cmd.ExecuteReader();
+reader.Read();
+Console.WriteLine(reader["Titolo"]);
+conn.Close();
